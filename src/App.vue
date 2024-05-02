c2a5d19999bd98a5f45b94f5dd43ed11b7706cb9
Camille Walbott <camille.walbott@gmail.com>
2024-05-02 18:11:25 +0200

    first

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+
+    <div>
+      <HelloWorld msg="hello bitches"/>
+    </div>
+  </header>
+
+  <main>
+  </main>
+</template>
