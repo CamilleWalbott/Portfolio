c93411d795f1242f3f59b7fb685da306ee9af91a
Camille Walbott <camille.walbott@gmail.com>
2025-02-09 19:52:10 +0100

    ajout flowbite + construction navbar

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import HelloWorld from './components/HelloWorld.vue'
 import { useI18n } from "vue-i18n";
 import { ref } from "vue";
+import NavBar from "./components/NavBar.vue";
 
 const { t, locale } = useI18n();
 const switchLang = () => {
@@ -11,15 +12,25 @@ const switchLang = () => {
 </script>
 
 <template>
-  <header>
-    <div>
-      <button @click="switchLang">🌍 {{ locale === "fr" ? "EN" : "FR" }}</button>
-    </div>
-    <div>
-      <HelloWorld :msg="t('welcome')"/>
-    </div>
-  </header>
+  <div class="min-h-screen w-full">
+    <!-- Header -->
+    <header class="bg-gray-800 text-white p-4">
+      <NavBar />
+    </header>
 
-  <main>
-  </main>
+    <!-- Main Content -->
+    <main class="flex-1 flex items-center justify-center">
+      <div class="text-center">
+        <button @click="switchLang" class="px-4 py-2 bg-blue-500 text-white rounded">
+          🌍 {{ locale === "fr" ? "EN" : "FR" }}
+        </button>
+        <div class="mt-4">
+          <HelloWorld :msg="t('welcome')" />
+        </div>
+      </div>
+    </main>
+  </div>
 </template>
+
+
+
