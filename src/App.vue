c5b7c3a9224b387840179e178dea8a4698bf8b1c
Camille Walbott <camille.walbott@gmail.com>
2025-02-10 03:40:55 +0100

    Modification navbar : ajout du module de changement de langue

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,13 @@ import NavBar from './components/NavBar.vue'
 import { useI18n } from "vue-i18n";
 import { ref } from "vue";
 
-const { t, locale } = useI18n();
-const switchLang = () => {
-  locale.value = locale.value === "fr" ? "en" : "fr";
-};
+const { t } = useI18n();
 
 </script>
 
 <template>
   <NavBar />
     <div>
-      <button @click="switchLang">🌍 {{ locale === "fr" ? "EN" : "FR" }}</button>
     </div>
     <div>
       <HelloWorld :msg="t('welcome')"/>
